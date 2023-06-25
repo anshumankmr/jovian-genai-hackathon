aff757e861f7333dc7b8c521871d7d1e35ab26e6
sibaspage <13863225+sibaspage@users.noreply.github.com>
2023-06-25 16:21:14 +0530

    header with search added

--- UI/src/app/modules/home/home.component.ts
+++ UI/src/app/modules/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  queryText = new FormControl();
+
+  constructor(public route: Router) { }
+
+  // search code
+  searchCode(): boolean {
+    if (this.queryText.value) {
+      this.route.navigate(
+        ['/search'],
+        { queryParams: { q: encodeURIComponent(this.queryText.value) } }
+      );
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- UI/src/app/shared/header/header.component.ts
+++ UI/src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  queryText = new FormControl();
+
+  constructor(public router: Router) {}
+
+  searchCode(): boolean {
+    if (this.queryText.value) {
+      this.router.navigate(
+        ['/search'],
+        { queryParams: { q: encodeURIComponent(this.queryText.value) } }
+      )
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
