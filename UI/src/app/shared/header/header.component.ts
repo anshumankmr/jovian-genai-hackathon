efb85449a8c8709debc89d3a451c732a31a5a4e9
sibaspage <13863225+sibaspage@users.noreply.github.com>
2023-06-25 16:34:46 +0530

    search footer updated

--- UI/src/app/shared/header/header.component.ts
+++ UI/src/app/shared/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,17 +11,30 @@ export class HeaderComponent {
 
   queryText = new FormControl();
 
-  constructor(public router: Router) {}
+  constructor(public router: Router, public activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params["q"].length) {
+        this.queryText.setValue(decodeURIComponent(params["q"]));
+      }
+    });
+  }
 
   searchCode(): boolean {
     if (this.queryText.value) {
       this.router.navigate(
         ['/search'],
         { queryParams: { q: encodeURIComponent(this.queryText.value) } }
-      )
+      );
       return true;
     } else {
       return false;
     }
+  }
+
+  clearSearch(): void {
+    this.queryText.setValue('');
+    this.router.navigate(['/search']);
   }
 }
